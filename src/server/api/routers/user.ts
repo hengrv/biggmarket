0b98b3989a5ebdcf6df527ed6e9bb9b885949b4e
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2025-02-08 16:15:40 +0000

    User route templates

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -7,11 +7,31 @@ import {
 } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  getProfile: protectedProcedure.query(async ({ ctx }) => {
-    const profile = await ctx.db.user.findUnique({
-      where: { id: ctx.session.user.id },
-    });
+    // * Get profile
+    getProfile: protectedProcedure.query(async ({ ctx }) => {
+        const profile = await ctx.db.user.findUnique({
+            where: { id: ctx.session.user.id },
+        });
 
-    return profile ?? null;
-  }),
+        return profile ?? null;
+    }),
+
+    // * Delete profile
+    deleteProfile: protectedProcedure.mutation(async ({ ctx }) => {
+        const profile = await ctx.db.user.delete({
+            where: { id: ctx.session.user.id },
+        });
+    
+        return profile ?? null;
+    }),
+
+    // ! TODO
+    // * Update profile
+    
+
+    // ! TODO
+    
 });
+
+
+
