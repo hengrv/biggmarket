b3da6c706f2b1645afd7fbcf5f135e848ecc9ada
henrygrv <hengro44@gmail.com>
2025-02-10 14:43:32 +0000

    feat: remove post logic and front end code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import { UserProfile } from "./_components/profile";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
-    void api.post.getLatest.prefetch();
   }
 
   return (
@@ -18,9 +15,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
+            <p className="text-2xl text-white"></p>
 
             <div className="flex flex-col items-center justify-center gap-4">
               <p className="text-center text-2xl text-white">
@@ -35,7 +30,6 @@ export default async function Home() {
             </div>
           </div>
 
-          {session?.user && <LatestPost />}
           {session?.user && <UserProfile />}
         </div>
       </main>
