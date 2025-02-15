2de3078e16bc1d8044d98a55c2f440b63e15c570
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2025-02-15 15:04:34 +0000

    API - User profile editing & profile reviews functionality (#8)
    * feat(db): added initial item and transaction model
    
    * Modify Transaction table and create relations with User and Item table
    
    * feature(db): removes post table and fix relation issuse
    
    * feature(db): added attributes to fix relationships
    
    * Modify User, Item, Transaction, and Log tables; Add enum for categories; fixed relationships between tables and add cascade deletes
    
    * merge databse files
    
    * Add swipes and matches field to Item and User tables
    
    * added comment
    
    * changed link between transactions and item
    
    * feature(db):adding swipes and matches model
    
    * chore: Add comments to tables
    
    * Cleaned whitespaces and table names for consistency
    
    * chore: fix item/user fields
    
    * patch: fix extra swipes field
    
    * feat: Added Item API, including CRUD Operations (#3)
    
    * Some template API routes
    
    Generated DB
    Added a api route for user & transaction.
    Added a test 'user' component in frontend to display user information.
    
    * Update user.ts
    
    * Remove console log line
    
    * User route templates
    
    * feat: remove post logic and front end code
    
    * feat(be): add item backend api
    
    - added getNextItem route - retrieve next item from db
    - added swipeItem route - adds a new swipe object when a user swipes
    - added getMatches route - gets a user's matches
    - added updateMatchStatus - mutation to change the state of a 'match'
    - added getSwipeStats - gives statistics about a user's swiping behaviour
    
    * added CRUD operations for itmes
    
    * added temporary component for testing of API
    
    * update tsconfig
    
    * added missing types
    
    ---------
    
    Co-authored-by: Will Forkes <35146567+WillForkes@users.noreply.github.com>
    
    * patch: fixed incorrect db table name in item router
    
    * patch(db): remove irrelevant field from user model
    
    * Update user profile route
    
    Added functionality so that the user can update their profile picture via a test component
    
    * Added review functionality
    
    - Added reviews table to db schema
    - Added api routes for adding reviews to other users & getting reviews on your profile
    
    * Update reviewtest.tsx
    
    * Refetch after new review added
    
    * Update profile edit extra functionality
    
    ---------
    
    Co-authored-by: robin <robin.husbands3411@gmail.com>
    Co-authored-by: darioLabrador <dariolabrador16@gmail.com>
    Co-authored-by: henrygrv <hengro44@gmail.com>

---
+++ prisma/migrations/20250211181023_patch_matches_col/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `matches` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "matches";
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -6,6 +6,18 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
+const userProfileInput = z.object({
+    image: z.string().url("Must be a valid URL").optional(),
+    email: z.string().email("Must be a valid email").optional(),
+    name: z.string().optional(),
+  });
+
+  const reviewInput = z.object({
+    userId: z.string(),
+    review: z.string(),
+    rating: z.number(),
+  })
+
 export const userRouter = createTRPCRouter({
     // * Get profile
     getProfile: protectedProcedure.query(async ({ ctx }) => {
@@ -17,20 +29,52 @@ export const userRouter = createTRPCRouter({
     }),
 
     // * Delete profile
-    deleteProfile: protectedProcedure.mutation(async ({ ctx }) => {
-        const profile = await ctx.db.user.delete({
+    // ! TODO
+
+    // * Update profile
+    updateProfile: protectedProcedure
+    .input(userProfileInput)
+    .mutation(async ({ ctx, input }) => {
+        const profile = await ctx.db.user.update({
             where: { id: ctx.session.user.id },
-        });
-    
+            data: {
+                ...input,
+            }
+        })
+
         return profile ?? null;
     }),
 
-    // ! TODO
-    // * Update profile
-    
+    // * Add review
+    addProfileReview: protectedProcedure
+    .input(reviewInput)
+    .mutation(async ({ ctx, input }) => {
+        const userId = ctx.session.user.id;
 
-    // ! TODO
+        const review = await ctx.db.review.create({
+            data: {
+              userId: input.userId, // This should be the user being reviewed
+              reviewer: userId, // Current user
+              review: input.review,
+              rating: input.rating,
+            },
+        })
+  
+      return review;
+    }),
     
+    // * Get reviews
+    getProfileReviews: protectedProcedure.query(async ({ ctx }) => {
+        const userId = ctx.session.user.id;
+
+        const reviews = await ctx.db.review.findMany({
+            where: {
+              userId,
+            },
+        })
+
+        return reviews;
+    }),
 });
 
 
