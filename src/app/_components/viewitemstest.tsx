e4c04441b1633620439081657a781ef34f8023ac
henrygrv <hengro44@gmail.com>
2025-02-20 11:15:24 +0000

    fixed if user has no location

--- src/app/_components/viewitemstest.tsx
+++ src/app/_components/viewitemstest.tsx
@@ -7,25 +7,42 @@ import Image from "next/image";
 export function ViewItemsTester() {
     // State
     const [userProfile] = api.user.getProfile.useSuspenseQuery();
-    const [items, { refetch: refetchItems }] = api.algorithm.getItemsByDistance.useSuspenseQuery({
-        longitude: Number(userProfile?.location?.longitude) ?? 0,
-        latitude: Number(userProfile?.location?.latitude) ?? 0,
-    });
+
+    let items, refetchItems;
+
+    if (userProfile?.location?.longitude) {
+        [items, { refetch: refetchItems }] =
+            api.algorithm.getItemsByDistance.useSuspenseQuery({
+                longitude: Number(userProfile?.location?.longitude) ?? 0,
+                latitude: Number(userProfile?.location?.latitude) ?? 0,
+            });
+    }
+
     const [errorMessage, setErrorMessage] = useState("");
 
     return (
         <div className="w-full max-w-xs">
             <div className="mb-8 rounded border p-4">
                 <h2 className="mb-4 text-xl font-semibold">View items near you</h2>
-                {items.map((item) => (
-                    <div key={item.id}>
-                        <Image src={item.image} width='100' height='100' alt="Item image" className="w-24 h-24" />
-                        <p className="truncate">ID: {item.id}</p>
-                        <p className="truncate">Description: {item.description}</p>
-                        <p className="truncate">Category: {item.category}</p>
-                        <p className="truncate">Distance: {Number(item.distance / 1000).toFixed(1)} Km</p>
-                    </div>
-                ))}
+                {items &&
+                    items.length > 0 &&
+                    items.map((item) => (
+                        <div key={item.id}>
+                            <Image
+                                src={item.image}
+                                width="100"
+                                height="100"
+                                alt="Item image"
+                                className="h-24 w-24"
+                            />
+                            <p className="truncate">ID: {item.id}</p>
+                            <p className="truncate">Description: {item.description}</p>
+                            <p className="truncate">Category: {item.category}</p>
+                            <p className="truncate">
+                                Distance: {Number(item.distance / 1000).toFixed(1)} Km
+                            </p>
+                        </div>
+                    ))}{" "}
             </div>
         </div>
     );
