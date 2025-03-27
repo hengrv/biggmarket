1c58722531faf958aeafc5375e9d00c19ece2ce6
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2025-03-27 14:48:59 +0000

    Added router for item upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -26,6 +26,28 @@ export const ourFileRouter = {
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId, fileUrl: file.ufsUrl };
     }),
+
+    itemImage: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const session = await auth();
+
+      // If you throw, the user will not be able to upload
+      if (!session || !session.user) throw new Error("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId, fileUrl: file.ufsUrl };
+    }),
 } satisfies FileRouter;
+
+
 
 export type OurFileRouter = typeof ourFileRouter;
