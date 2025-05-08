21e75567826e4968fb4683cab6dc77ad63de7dd8
hengrv <hengro44@gmail.com>
2025-05-08 15:25:20 +0100

    fix(fe): protect admin route from unauthorised useres

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,15 @@
 import AdminDashboard from "@/components/admin-dashboard";
+import { auth } from "~/server/auth";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 export default function AdminPage() {
+  const session = await auth();
+
+  // If user is not authenticated, redirect to login page
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return <AdminDashboard />;
 }
